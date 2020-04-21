2572ff10993951326be681d47e13801597c72547
Coffey <lucascoffey@gmail.com>
2020-04-21 15:17:47 -0400

    Initial commit

---
+++ state_providence_list.py
@@ -0,0 +1,56 @@
+country = ["Canada", "United States"]
+
+
+province = [
+    "Alberta",
+    "British Columbia",
+    "Manitoba",
+    "New Brunswick",
+    "Nova Scotia",
+    "Northwest Territory",
+    "Nunavut",
+    "Ontario",
+    "Quebec",
+    "Yukon"
+]
+
+state = [
+    "Alabama",
+    "Alaska",
+    "American Samoa",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "District of Colombia",
+    "Florida",
+    "Georgia",
+    "Guam",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    
+
+]
